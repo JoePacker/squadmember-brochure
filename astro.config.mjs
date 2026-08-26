71d316a5669f84d6ba99df420de080e2253bc116
Adam Packer <adam@s8080.com>
2026-08-26 17:04:28 +0100

    adding oswald secondary font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    fonts: [{
-        provider: fontProviders.google(),
-        name: 'Lexend',
-        cssVariable: '--font-lexend',
-        subsets: ['latin'],
-        fallbacks: ['sans-serif'],
-    }]
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Lexend",
+      cssVariable: "--font-lexend",
+      subsets: ["latin"],
+      fallbacks: ["sans-serif"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Oswald",
+      cssVariable: "--font-oswald",
+      subsets: ["latin"],
+      fallbacks: ["sans-serif"],
+    },
+  ],
 });
